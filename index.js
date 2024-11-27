c93944db6f7e915c1fe9166c0541f0eb6c0a8352
Dmitriy Mezhevich <dimchik843@gmail.com>
2024-11-27 12:40:37 +0300

    add express

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require('express');
+
 const bot = require('./telegramBot/bot');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 bot.start();
